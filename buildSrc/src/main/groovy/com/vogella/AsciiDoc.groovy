b3123836f20f0a3b85e423c582c3842c11f2f9eb
Lars Vogel <Lars.Vogel@vogella.com>
2016-01-06 17:40:29 +0100

    Fixes Gradle build task

--- buildSrc/src/main/groovy/com/vogella/AsciiDoc.groovy
+++ buildSrc/src/main/groovy/com/vogella/AsciiDoc.groovy
@@ -12,12 +12,10 @@ class AsciiDoc extends AsciidoctorTask {
 
 		options doctype: 'book'
 
-		attributes	
-		'source-highlighter' : 'coderay',
+		attributes	'source-highlighter' : 'coderay',
 		'toc':'',
 		'icons': 'font',
 		'setanchors':'true',
-		'docinfo1': '',
 		'idprefix':'',
 		'idseparator':'-',
 		'docinfo1':'true'
@@ -37,3 +35,4 @@ class AsciiDoc extends AsciidoctorTask {
 		)
 	}
 }
+
