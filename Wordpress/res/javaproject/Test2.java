3395d5af2ec6cab535088e622d6af9093bd4261c
Lars Vogel <Lars.Vogel@vogella.com>
2018-01-25 14:59:36 +0100

    Updates of the technology tutorials

---
+++ Wordpress/res/javaproject/Test2.java
@@ -0,0 +1,15 @@
+				
+package de.vogella.mercurial.firstproject;
+
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Mercurial");
+		System.out.println("This is a new class");
+	}
+
+}
+
---
+++ Wordpress/res/javaproject/TestNewName.java
@@ -0,0 +1,13 @@
+			
+package de.vogella.mercurial.firstproject;
+
+public class TestNewName {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Mercurial from the new name");
+	}
+
+}
